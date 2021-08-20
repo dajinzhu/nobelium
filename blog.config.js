a443a6b30d9c6b4e98185926c0572cad10ca6154
dajinzhu <41352123+dajinzhu@users.noreply.github.com>
2021-08-20 21:01:47 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,9 +17,9 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twitter.com/craigaryhart',
+  socialLink: 'https://twitter.com/chenxi2007',
   seo: {
-    keywords: ['Blog', 'Website', 'Notion'],
+    keywords: ['陈锡博客', '陈锡', '陈锡个人博客'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
